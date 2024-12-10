8099eae18c3aa5df469cbabc77042d62cd8eb6a8
Dudeszkombo <168413684+Dudeszkombo@users.noreply.github.com>
2024-12-10 19:35:25 +0100

    Add files via upload

---
+++ scripts/collectables/CollectCoin.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoin : MonoBehaviour
+{
+    public AudioSource coinSFX;
+    void OnTriggerEnter(Collider other)
+    {
+        coinSFX.Play();
+        CollectableControl.coinCount += 1;
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ scripts/enviroment/ObstacleCollision.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCollision : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public GameObject charModel;
+    public AudioSource crashThud;
+    public GameObject mainCam;
+    public GameObject LevelControl;
+    void OnTriggerEnter(Collider other)
+    {
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        thePlayer.GetComponent<PlayerMove>().enabled = false;
+        charModel.GetComponent<Animator>().Play("Falling Back Death");
+        LevelControl.GetComponent<LevelDistance>().enabled = false;
+        crashThud.Play();
+        mainCam.GetComponent<Animator>().enabled = true;
+        LevelControl.GetComponent<EndRunSequence>().enabled = true;
+    }
+}
